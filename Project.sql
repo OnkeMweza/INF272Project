9f830dffcf8846dba8226227f26800cc5e491dc8
u18059326 <53194386+u18059326@users.noreply.github.com>
2019-10-21 21:08:27 +0200

    Add files via upload
    Final

--- Project.sql
+++ Project.sql
@@ -8,12 +8,10 @@ drop table SignInDetails;
 drop table PersonalDetails;
 drop table Addresses;
 
-/*create table SignInDetails(
-EmailAddress varchar(50) primary key,
-Firstname varchar(50),
-Surname varchar(50),
-Upassword varchar(100)
-);*/
+create table Medical_Aid(
+MID int identity(1,1) primary key,
+MedicalAidName varchar(50)
+);
 
 create table Vital(
 VitalID int identity(1,1) primary key, 
@@ -24,18 +22,9 @@ IronLevels int,
 WhiteBloodCells int, 
 RedBloodCells int,
 Antibodies int,
-IronLevel int,
- )
-
-/*create table PersonalDetails(
-IDNumber varchar(20) primary key,
-Age int not null,
-ContactDetails varchar(20),
-EmailAddress varchar(50),
-/*Still have to add address*/
-AddressID int,
-foreign key (EmailAddress) references SignInDetails(EmailAddress),
-);*/
+Glucose int,
+Cholesterol int
+ );
 
 create table UserType(
 UTID int identity(1,1) primary key,
@@ -82,9 +71,10 @@ foreign key (StreetID) references Street(StreetID)
 );
 
 create table Patient(
+PatientID int identity(1,1) primary key,
 FirstName varchar(50),
 Surname varchar(50),
-EmailAddress varchar(50) primary key,
+EmailAddress varchar(50) unique,
 Age int,
 ContactDetails char(10),
 ChipID char(10),
@@ -105,7 +95,8 @@ foreign key (AID) references UAddress(AID)
 );
 
 create table SysAdmin(
-EmailAddress varchar(50) primary key,
+AdminID int identity(1,1) primary key,
+EmailAddress varchar(50) unique,
 FirstName varchar(50),
 Surname varchar(50),
 ContactDetails char(10)
@@ -121,7 +112,18 @@ foreign key (UTID) references UserType(UTID),
 foreign key(EmailAddress) references Patient(EmailAddress)/*Not sure about this because there's 2 tables, Patients and Admins**/
 );
 
+create table Hospital(
+HID int identity(1,1) primary key,
+HospitalName varchar(50),
+AID int,
+foreign key (AID) references UAddress(AID)
+);
 
+create table Partner(
+PID int identity(1,1) primary key,
+PartnerName varchar(50),
+ContactDetails char(10)
+);
 
 
 /*Alter table AppUser add constraint EmailAddress foreign key(EmailAddress) references Patient(EmailAddress) ON DELETE CASCADE    
