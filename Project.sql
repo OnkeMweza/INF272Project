a2c48f43030496f2821fc57c9482bbb3bb81c5dc
u18059326 <53194386+u18059326@users.noreply.github.com>
2019-10-21 20:39:53 +0200

    Add files via upload
    Two tables missing

--- Project.sql
+++ Project.sql
@@ -8,12 +8,12 @@ drop table SignInDetails;
 drop table PersonalDetails;
 drop table Addresses;
 
-create table SignInDetails(
+/*create table SignInDetails(
 EmailAddress varchar(50) primary key,
 Firstname varchar(50),
 Surname varchar(50),
 Upassword varchar(100)
-);
+);*/
 
 create table Vital(
 VitalID int identity(1,1) primary key, 
@@ -21,13 +21,13 @@ HeartRate int,
 BloodPressure int,
 BloodSugar int,
 IronLevels int,
-WhiteBlood Cell int, 
-RedBlood Cell int,
+WhiteBloodCells int, 
+RedBloodCells int,
 Antibodies int,
 IronLevel int,
  )
 
-create table PersonalDetails(
+/*create table PersonalDetails(
 IDNumber varchar(20) primary key,
 Age int not null,
 ContactDetails varchar(20),
@@ -35,18 +35,95 @@ EmailAddress varchar(50),
 /*Still have to add address*/
 AddressID int,
 foreign key (EmailAddress) references SignInDetails(EmailAddress),
+);*/
+
+create table UserType(
+UTID int identity(1,1) primary key,
+UTDescription varchar(50)
 );
 
-Create table Address(
-AddressID int identity(1,1) primary key,
-Province varchar(20),
-City varchar(20),
-StreetName varchar(20),
-HouseNumber varchar(20),
-IDNumber varchar(20),
-foreign key (IDNumber) references PersonalDetails(IDNumber)
+insert into UserType(UTDescription)
+values
+('Patient'),
+('Admin');
+
+create table PatientRelative(
+RID int identity(1,1) primary key,
+FirstName varchar(50),
+Surname varchar(50),
+ContactDetailsP char(10),
+ContactDetailsH char(10),
+ContactDetailsW char(10),
 );
 
-Alter table PersonalDetails add constraint AddressID foreign key(AddressID) references Addresses(AddressID) ON DELETE CASCADE    
-      ON UPDATE CASCADE;
+create table Illness(
+IID int identity(1,1) primary key,
+IllnessName varchar(50)
+);
+
+create table Surbub(
+SurbubID int identity(1,1) primary key,
+SurbubName varchar(50),
+);
+
+create table Street(
+StreetID int identity(1,1) primary key,
+StreetName varchar(50),
+HouseNO int,
+SurbubID int,
+foreign key (SurbubID) references Surbub(SurbubID)
+
+);
+
+Create table UAddress(
+AID int identity(1,1) primary key,
+StreetID int,
+foreign key (StreetID) references Street(StreetID)
+);
+
+create table Patient(
+FirstName varchar(50),
+Surname varchar(50),
+EmailAddress varchar(50) primary key,
+Age int,
+ContactDetails char(10),
+ChipID char(10),
+/*IllnessID FK
+MedAid FK
+Vitals
+Relatives
+Address*/
+
+VitalID int,
+foreign key (VitalID) references Vital(VitalID),
+RID int,
+foreign key (RID) references PatientRelative(RID),
+IID int,
+foreign key (IID) references Illness(IID),
+AID int,
+foreign key (AID) references UAddress(AID)
+);
+
+create table SysAdmin(
+EmailAddress varchar(50) primary key,
+FirstName varchar(50),
+Surname varchar(50),
+ContactDetails char(10)
+)
+
+create table AppUser(
+userID int identity(1,1),
+EmailAddress varchar(50),	/*Foreign key*/
+Upassword varchar(50),
+/*Foreign key to usertype table*/
+UTID int,
+foreign key (UTID) references UserType(UTID),
+foreign key(EmailAddress) references Patient(EmailAddress)/*Not sure about this because there's 2 tables, Patients and Admins**/
+);
+
+
+
+
+/*Alter table AppUser add constraint EmailAddress foreign key(EmailAddress) references Patient(EmailAddress) ON DELETE CASCADE    
+      ON UPDATE CASCADE;*/
 
