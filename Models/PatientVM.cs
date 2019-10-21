0cf4d0b57ce4039488adbe46c27e56d773cfc63e
u18319425 <50372688+u18319425@users.noreply.github.com>
2019-10-22 01:04:34 +0200

    Add files via upload

---
+++ Models/PatientVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Deliverable2.Models
+{
+    public class PatientVM
+    {
+        public int? id;
+        public List<Patient> patients;
+        public UserVM userVM;
+    }
+}
